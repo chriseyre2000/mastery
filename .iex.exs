bee03367b490cc2dcee15d0807d58d720c6dbcc4
Christopher Eyre <chriseyre2000@gmail.com>
2020-03-14 14:16:42 +0000

    Added initial tests

---
+++ .iex.exs
@@ -0,0 +1,11 @@
+alias Mastery.Core.{Template, Quiz, Response}
+generator = %{left: [1, 2], right: [1, 2] }
+checker = fn(sub, answer) -> sub[:left] + sub[:right] == String.to_integer(answer) end
+quiz = Quiz.new(title: "Addition", mastery: 2) 
+       |> Quiz.add_template( name: :single_digit_addition, 
+                             category: :addition,
+                             instructions: "Add the numbers",
+                             raw: "<%= @left %> + <%= @right %>",
+                             generators: generator,
+                             checker: checker )
+       |> Quiz.select_question
