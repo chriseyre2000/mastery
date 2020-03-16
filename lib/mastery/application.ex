c277075af490542949b5cb9d668d2e7531e49ed4
Christopher Eyre <chriseyre2000@gmail.com>
2020-03-16 21:24:57 +0000

    End of Chapter 7

--- lib/mastery/application.ex
+++ lib/mastery/application.ex
@@ -1,18 +1,26 @@
 defmodule Mastery.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: Mastery.Worker.start_link(arg)
-      # {Mastery.Worker, arg}
+      { Mastery.Boundary.QuizManager, 
+        [name: Mastery.Boundary.QuizManager]
+      }, 
+      {
+        Registry,
+        [ 
+          name: Mastery.Registry.QuizSession,
+          keys: :unique
+        ]
+      },
+      {
+        DynamicSupervisor,
+        [name: Mastery.Supervisor.QuizSession, strategy: :one_for_one]
+      }
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Mastery.Supervisor]
     Supervisor.start_link(children, opts)
   end
--- lib/mastery/boundary/quiz_session.ex
+++ lib/mastery/boundary/quiz_session.ex
@@ -2,6 +2,27 @@ defmodule Mastery.Boundary.QuizSession do
   alias Mastery.Core.{Quiz, Response}
   use GenServer
 
+  def child_spec({quiz, email}) do
+    %{
+      id: {__MODULE__, {quiz.title, email}},
+      start: {__MODULE__, :start_link, [{quiz, email}]},
+      restart: :temporary
+    }
+  end
+  
+  def start_link({quiz, email}) do
+    GenServer.start_link(__MODULE__, 
+        {quiz, email},
+        name: via({quiz.title, email}))
+  end
+  
+  def take_quiz(quiz, email) do
+    DynamicSupervisor.start_child(
+      Mastery.SuperVisor.QuizSession,
+      {__MODULE__, {quiz, email}}
+    )
+  end  
+
   def init({quiz, email}) do
     {:ok, {quiz, email}}
   end
@@ -28,11 +49,19 @@ defmodule Mastery.Boundary.QuizSession do
   end
   
   ## API
-  def select_question(session) do
-    GenServer.call(session, :select_question)
+  def select_question(name) do
+    GenServer.call(via(name), :select_question)
   end
   
-  def answer_question(session, answer) do
-    GenServer.call(session, {:answer_question, answer})
+  def answer_question(name, answer) do
+    GenServer.call(via(name), {:answer_question, answer})
+  end 
+  
+  def via({_title, _email} = name) do
+    {
+      :via,
+      Registry,
+      {Mastery.Registry.QuizSession, name}
+    }
   end  
 end
