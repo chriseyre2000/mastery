c1d9530b01e84db7a4cf4d3cee04f1a68c2b32b5
Christopher Eyre <chriseyre2000@gmail.com>
2020-03-16 18:41:19 +0000

    Minimal boundary example

--- .iex.exs
+++ .iex.exs
@@ -1,11 +1,3 @@
-alias Mastery.Core.{Template, Quiz, Response}
-generator = %{left: [1, 2], right: [1, 2] }
-checker = fn(sub, answer) -> sub[:left] + sub[:right] == String.to_integer(answer) end
-quiz = Quiz.new(title: "Addition", mastery: 2) 
-       |> Quiz.add_template( name: :single_digit_addition, 
-                             category: :addition,
-                             instructions: "Add the numbers",
-                             raw: "<%= @left %> + <%= @right %>",
-                             generators: generator,
-                             checker: checker )
-       |> Quiz.select_question
+alias Mastery.Examples.Maths
+alias Mastery.Boundary.QuizManager
+
