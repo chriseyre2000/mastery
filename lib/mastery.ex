eebc8b23c8c78048b4d22fad399e8f1963c02d7a
Christopher Eyre <chriseyre2000@gmail.com>
2020-03-16 20:24:54 +0000

    Added basic external api

--- lib/mastery.ex
+++ lib/mastery.ex
@@ -1,18 +1,39 @@
 defmodule Mastery do
-  @moduledoc """
-  Documentation for Mastery.
-  """
+  alias Mastery.Boundary.{QuizSession, QuizManager}
+  alias Mastery.Boundary.{TemplateValidator, QuizValidator}
+  alias Mastery.Core.Quiz
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Mastery.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def start_quiz_manager() do
+    GenServer.start_link(QuizManager, %{}, name: QuizManager)
   end
+  
+  def build_quiz(fields) do
+    with :ok <- QuizValidator.errors(fields),
+         :ok <- GenServer.call(QuizManager, {:build_quiz, fields}),
+    do: :ok, else: (error -> error)
+  end
+  
+  def add_template(title, fields) do
+    with :ok <- TemplateValidator.errors(fields),
+         :ok <- GenServer.call(QuizManager, {:add_template, title, fields}),
+         do: :ok, else: (error -> error)
+  end
+  
+  def take_quiz(title, email) do
+    with %Quiz{} = quiz <- QuizManager.lookup_quiz_by_title(title),
+         {:ok, session} <- GenServer.start_link(QuizSession, {quiz, email})
+         do
+          session
+         else
+           error -> error
+         end
+  end
+
+  def select_question(session) do
+    GenServer.call(session, :select_question)
+  end
+
+  def answer_question(session, answer) do
+    GenServer.call(session, {:answer_question, answer})
+  end  
 end
